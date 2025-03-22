5d90360c06cc6fe0516a1c320faf29986a7646f1
Timcodingeur <124517012+Timcodingeur@users.noreply.github.com>
2025-03-22 09:49:35 +0100

    faire fonctionner le backend

---
+++ app/backend/src/config/database.mjs
@@ -0,0 +1,21 @@
+import mysql from 'mysql2';
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+};
+
+const connection = mysql.createConnection(dbConfig);
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.message);
+    return;
+  }
+  console.log('Successfully connected to the database');
+});
+
+export default connection;
